a873167d3f5de0a9e600d475ca886a98cdcda615
Satish Nikale <satishnikale7@gmail.com>
2024-10-20 12:02:53 +0530

    Fix some issues

--- routes/user.js
+++ routes/user.js
@@ -2,32 +2,32 @@ const { Router } = require("express");
 const bcrypt = require("bcrypt");
 const userRouter = Router();
 const { z } = require("zod");
-
 const { userModel, purchaseModel, courseModel } = require("../db");
 
+    
     userRouter.post("/signup", async function(req, res){
         const requiredBody = z.object({
             email : z.string().min(4).max(25).email(),
-            password :z.string().min(6).max(18).password(),
+            password :z.string().min(6).max(18),
             firstName : z.string().min(4).max(25),
             lastName : z.string().min(4).max(25)
         });
-        // const parsedData = requiredBody.parse(req.body);
-        const parsedWithSuccess = requiredBody.safeParse(req.body);
 
-        if(!parsedWithSuccess.success){
-            res.json({
-                message : "Invalide input",
-                error : parsedWithSuccess.error
-            });
-        }
-
-        const {email, password, firstName, lastName } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 5);
+    // const parsedData = requiredBody.parse(req.body);
+    const parsedWithSuccess = requiredBody.safeParse(req.body);
+    if(!parsedWithSuccess.success){
+        res.json({
+            message : "Invalide input",
+            error : parsedWithSuccess.error
+        });
+        return
+    }
+    const {email, password, firstName, lastName } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 5);
 
        try {
         await userModel.create({
-            email :email,
+            email :email.toLowerCase(),
             password : hashedPassword,
             firstName : firstName,
             lastName : lastName
